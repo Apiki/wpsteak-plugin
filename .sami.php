3a3f28487051ae2d2c901a0e6e5dfb3bd0afb681
Elvis Henrique Pereira <elvishp2006@gmail.com>
2019-01-11 08:42:20 -0200

    Format sami and add more things on .gitattributes

--- .sami.php
+++ .sami.php
@@ -1,8 +1,9 @@
 <?php
 /**
+ * Sami
+ *
+ * @link https://github.com/FriendsOfPHP/Sami
  * @package   App
- * @author    Apiki
- * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0
  */
 
 use Sami\RemoteRepository\GitHubRemoteRepository;
@@ -11,20 +12,23 @@ use Sami\Parser\Filter\PublicFilter;
 
 define( 'DS', DIRECTORY_SEPARATOR );
 
-$repository = 'apiki/wpsteak-plugin';
-$relative_dir = 'src';
-$dir = __DIR__ . DS . $relative_dir;
+$repository        = 'apiki/wpsteak-plugin';
+$relative_dir      = 'src';
+$dir               = __DIR__ . DS . $relative_dir;
 $documentation_dir = __DIR__ . DS . 'api-reference';
 
-$versions = GitVersionCollection::create($dir)
-	->addFromTags('*.*.*')
-	->add('master', 'master branch');
+$versions = GitVersionCollection::create( $dir )
+	->addFromTags( '*.*.*' )
+	->add( 'master', 'master branch' );
 
-return new \Sami\Sami( $dir, [
-	'title' => 'WP Steak',
-	'versions' => $versions,
-	'remote_repository' => new GitHubRemoteRepository( $repository, dirname( $dir ) ),
-	'build_dir' => $documentation_dir . DS . 'build' . DS . '%version%',
-	'cache_dir' => $documentation_dir . DS . 'cache' . DS . '%version%',
-	'filter' => new PublicFilter(),
-] );
+return new \Sami\Sami(
+	$dir,
+	[
+		'title'             => 'WP Steak',
+		'versions'          => $versions,
+		'remote_repository' => new GitHubRemoteRepository( $repository, dirname( $dir ) ),
+		'build_dir'         => $documentation_dir . DS . 'build' . DS . '%version%',
+		'cache_dir'         => $documentation_dir . DS . 'cache' . DS . '%version%',
+		'filter'            => new PublicFilter(),
+	]
+);

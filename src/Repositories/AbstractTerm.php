604f1521079788060b95a458e5f92c7254026112
Elvis Henrique Pereira <elvishp2006@gmail.com>
2019-01-17 22:22:24 -0200

    Fix typehint.

--- src/Repositories/AbstractTerm.php
+++ src/Repositories/AbstractTerm.php
@@ -51,7 +51,7 @@ abstract class AbstractTerm {
 	 * Get terms.
 	 *
 	 * @param array $args Args.
-	 * @return \WP_Error|\WP_Term[]
+	 * @return \WP_Error|array
 	 * @throws \InvalidArgumentException When the passed taxonomy does not exists.
 	 */
 	protected function get_terms( array $args ) : array {

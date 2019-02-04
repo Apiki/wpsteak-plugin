03f87f7eb08c3db40851a8140a72627dc382a275
Elvis Henrique Pereira <elvishp2006@gmail.com>
2019-02-04 13:25:27 -0200

    Remove favicon.

--- resources/scripts/theme/index.js
+++ resources/scripts/theme/index.js
@@ -3,8 +3,6 @@ import 'airbnb-browser-shims';
 // eslint-disable-next-line no-unused-vars
 import config from '@config';
 
-import '@images/favicon.ico';
-
 import './vendor/*.js';
 
 // Your code goes here ...
